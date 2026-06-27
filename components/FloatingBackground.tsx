cf909757ca21475e94b0c15fa6c2281566320527
faresse26 <prvtfares03@gmail.com>
2026-06-27 20:44:08 +0100

    upload: components/FloatingBackground.tsx

---
+++ components/FloatingBackground.tsx
@@ -0,0 +1,11 @@
+export default function FloatingBackground() {
+  return (
+    <div className="bg-canvas" aria-hidden="true">
+      <div className="bg-orb bg-orb-1" />
+      <div className="bg-orb bg-orb-2" />
+      <div className="bg-orb bg-orb-3" />
+      <div className="bg-orb bg-orb-4" />
+      <div className="bg-orb bg-orb-5" />
+    </div>
+  );
+}
